6390a2508eeb1b5aa0d332d074bcde53fbde6a55
Amin Gharibi <60180731+Amin-Gharibi@users.noreply.github.com>
2023-11-13 00:53:17 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const numberOfStudents = +prompt("Please Enter Number of Students you want to enter their grade:")
+let studentGrade;
+let gradesSum = 0;
+let finalAnswer;
+
+if (isNaN(numberOfStudents) || (numberOfStudents <= 0)) {
+    alert("Please Enter a correct number!")
+} else {
+    for (let i = 0; i < numberOfStudents; i++) {
+        studentGrade = +prompt("please enter the grade of student:")
+        if (isNaN(studentGrade) || (studentGrade < 0)) {
+            alert('Please enter a correct amount of number!')
+            i--
+        } else {
+            gradesSum += studentGrade
+        }
+    }
+
+    finalAnswer = gradesSum / numberOfStudents
+}
+
+alert(`avg grage is: ${finalAnswer}`)
